0835b00095f2a673a9547dd009c9935b3fc92757
HP <luzzigeraldo@gmail.com>
2024-04-26 15:06:30 +0300

    Finalize and beginning image search App

--- SearchApp/script2.js
+++ SearchApp/script2.js
@@ -1,6 +1,24 @@
 const apiKey = '80178d7ee86d45f2ab0db47b3d24e760'
 
 const blogContainer = document.getElementById("bolg-container");
+// first time browsing.
+async function fetchRandomNews()
+{
+    try{
+
+        const apiUrl = https://newsapi.org/v2/top-headlines?country=us&pageSize=40&apikey=${apiKey};
+        
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data)
+        return data.articles;
+
+    }catch(error){
+        console.error("Error fetching random news",error)
+        return[];
+    }
+}
+
 function displayBlogs(articles){
     blogContainer.innerHTML = "";
     articles.forEach((article) => {
@@ -18,10 +36,8 @@ function displayBlogs(articles){
         title.textContent = truncatedTitle;
         
         const description = document.createElement("p");
-        const truncatedDes = 
-            article.description.length >120
-               ? article.description.slice(0,120) + "...."
-                :article.description;
+        // nimetoa truncation ya description kwa sababu in some cases description.legth inaleta error sijui mbona but hiyo ni wewe ufind out
+        const truncatedDes = article.description;
 
         description.textContent = truncatedDes;
 
@@ -50,5 +66,5 @@ function displayBlogs(articles){
     }catch(error){
         console.error("Error fetching random news",error);
 
-    }
-}) ();
+    }
+}) ();
